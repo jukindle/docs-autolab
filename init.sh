22d1d2159362527756b3481f5d4555fac9d82dce
Julien Kindle <jkindle@student.ethz.ch>
2018-07-20 12:38:55 +0200

    m

---
+++ init.sh
@@ -0,0 +1,7 @@
+git submodule sync --recursive
+git submodule update --init --recursive
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update;	apt-cache search docker-ce; sudo apt-get install docker-ce
+make install-docker-ubuntu16
