8b3aacb744cdecb564f815539e9921d60ff78d3b
Julien Kindle <jkindle@student.ethz.ch>
2018-07-20 13:17:24 +0200

    Movin

--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update;	apt-cache search docker-ce; sudo apt-get install docker-ce
 make install-docker-ubuntu16
+
+clear
+
+echo "Please sign out and sign in now in order to update the system groups. Then, you should be ready to compile with 'make compile'!"
